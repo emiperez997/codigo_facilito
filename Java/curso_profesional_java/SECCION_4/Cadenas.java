cdd3d018c1b35a8d79b14466123c1395cce17b76
Emiliano <emi.perez997@gmail.com>
2023-07-12 20:56:42 -0300

    [ADD] Curso Java

---
+++ Java/curso_profesional_java/SECCION_4/Cadenas.java
@@ -0,0 +1,19 @@
+package SECCION_4;
+
+class Cadenas {
+  public static void main(String[] args) {
+    // String
+    // Es una clase que representa una cadena de caracteres, no es un tipo de dato
+    // primitivo
+
+    // String mensaje;
+    // mensaje = "Hola Mundo";
+
+    String mensaje = "Hola Mundo";
+    String mensaje2 = new String("Hola Mundo 2");
+
+    System.out.println(mensaje);
+    System.out.println(mensaje2);
+
+  }
+}
---
+++ Java/curso_profesional_java/SECCION_4/MetodosString.java
@@ -0,0 +1,25 @@
+package SECCION_4;
+
+public class MetodosString {
+
+  public static void main(String[] args) {
+    // Métodos → Funciones que tiene la clase String
+    String mensaje = "Hola, soy un String";
+
+    int cantidad = mensaje.length(); // Devuelve la longitud del String en Int
+
+    // Devuelve un booleano si el String contiene la palabra "Hola"
+    boolean contiene = mensaje.contains("Hola");
+
+    boolean comienzaCon = mensaje.startsWith("H");
+    boolean terminaCon = mensaje.endsWith("g");
+
+    String nuevoStrings = mensaje.concat(". Adios"); // No modifica el String original
+
+    System.out.println(cantidad);
+    System.out.println(contiene);
+    System.out.println(comienzaCon);
+    System.out.println(terminaCon);
+    System.out.println(nuevoStrings);
+  }
+}
